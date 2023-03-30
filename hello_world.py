179de02b4938673b77f898f9eda2496273aff647
Shakshi Bandral <uietme2024shakshibandral@kuk.ac.in>
2023-03-30 22:58:41 +0530

    Adding new print line

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
 print("HelloWorld!!")
 print("Hello World!!, This is Sakshi Bandral!")
+class person:
+    def  __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def showinfo(self):
+        print("name",self.name)
+        print("age",self.age)
+        return
+p1=person("sakshi",45)
+p1.showinfo()
