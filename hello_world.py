1e79ed798e98cafa189b4c0bce05444f7c8dfcf4
Shakshi Bandral <uietme2024shakshibandral@kuk.ac.in>
2023-03-30 23:22:40 +0530

    Adding new print line

--- hello_world.py
+++ hello_world.py
@@ -10,3 +10,23 @@ class person:
         return
 p1=person("sakshi",45)
 p1.showinfo()
+
+
+class laptop:
+    def  __init__(self,company,core,):
+        self.core=core
+        self.company=company
+        return
+    def detail(self):
+        print("core",self.core)
+        print("company",self.company)
+        return
+l1=laptop("i5","asus")
+l1.detail()
+
+# list:
+a=["hii!!","how are you"]
+print(type(a))
+print(a)
+a[1]="i am good"
+print(a)
